e38de7f3d467543fcbcfa1aaffc55ed8b11b5618
lnoueryo <popo62520908@gmail.com>
2021-02-07 02:41:49 +0900

    必要ないコメントアウト削除

--- components/templates/Home.vue
+++ components/templates/Home.vue
@@ -8,26 +8,14 @@
                 みんなの好きな作品
             </v-col>
         </v-row>
-        <!-- <v-row>
-            <v-col>
-                {{ movie.title }}
-            </v-col>
-        </v-row> -->
         <v-row v-if="ready">
             <v-col v-for="movie in movies" :key="movie.id">
-                 <v-card 
-              
-                 class="d-flex"
-                 >
+                <v-card class="d-flex">
                     <div>
                       <v-img v-bind:src="'http://image.tmdb.org/t/p/w154/' + movie.poster_path"></v-img>
                     </div>
                     <div>
-                        <!-- <v-card-title>{{ movie.title }}</v-card-title>
-                        <v-card-subtitle>{{ movie.release_date }}</v-card-subtitle>
-                        <v-card-text>{{ movie.overview }}</v-card-text> -->
                         <card-template :title="movie.title" :release_date="movie.release_date" :overview="movie.overview" />
-                        <!-- v-bind:hoge="data" hogeは任意の名前、dataはdata(),computed,methods()からの値を入れることができる。 -->
                     </div>
                 </v-card>
             </v-col>
@@ -46,7 +34,6 @@ export default {
     data() {
         return {
             movies: [],
-            // movie: [],
             apiKey: 'a1a357b8cd4732e4d9c84ecc9a1d7406',
             ready: false,
         }
